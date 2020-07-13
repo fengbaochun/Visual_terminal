437623e3012d1fb4cdbbaa782e628fc6e2ca333c
fengbaochun <849067797@qq.com>
2020-07-13 18:46:52 +0800

    修改同类多控件多次调用槽函数问题

--- find_color_block.py
+++ find_color_block.py
@@ -13,14 +13,6 @@ from Ui_find_color_block import Ui_find_color_block
 from Color_recognition.Color_block_recogn import Color_block_recogn
 from Camera import Cam_dev
 
-# 目标颜色
-# tar_color = 'red'
-
-# color_dict ={   'red':      [127,197,188,60,197,255],
-#                 'blue':     [50,197,188,80,197,255],
-#                 'yellow':   [60,197,188,100,197,255],
-#               }   
-
 tar_color = 'red'
 
 color_dict ={   'red':      [127,197,188,60,197,255],
@@ -28,19 +20,24 @@ color_dict ={   'red':      [127,197,188,60,197,255],
                 'yellow':   [60,197,188,100,197,255],
               }   
 
-red_hsv=[127,197,188,60,197,255]
-blue_hsv=[50,197,188,80,197,255]
-yellow_hsv=[60,197,188,100,197,255]
+
 
 
 class Find_color_block(QtWidgets.QWidget, Ui_find_color_block):
+    # red_hsv=[127,197,188,60,197,255]
+    # blue_hsv=[50,197,188,80,197,255]
+    # yellow_hsv=[60,197,188,100,197,255]
     hsv=[]
+    num=0
     '''初始化'''
     def __init__(self):
         super(Find_color_block, self).__init__()
         self.setupUi(self)
-
-        #滑块测试
+        self.red_hsv=[127,197,188,60,197,255]
+        self.blue_hsv=[50,197,188,80,197,255]
+        self.yellow_hsv=[60,197,188,100,197,255]
+        
+        #滑块范围
         self.H_Slider_max.setRange(0,255)
         self.H_Slider_min.setRange(0,255)
         self.S_Slider_max.setRange(0,255)
@@ -48,6 +45,19 @@ class Find_color_block(QtWidgets.QWidget, Ui_find_color_block):
         self.V_Slider_max.setRange(0,255)
         self.V_Slider_min.setRange(0,255)
 
+        # 设置默认
+        self.checkBox_Red.setCheckState(Qt.Checked)   
+
+        # 设置默认为红色
+        self.hsv=self.red_hsv
+        self.H_Slider_max.setValue(self.hsv[0])
+        self.H_Slider_min.setValue(self.hsv[1])
+        self.S_Slider_max.setValue(self.hsv[2])
+        self.S_Slider_min.setValue(self.hsv[3])
+        self.V_Slider_max.setValue(self.hsv[4])
+        self.V_Slider_min.setValue(self.hsv[5])
+
+        # 槽函数
         self.H_Slider_max.valueChanged.connect(self.Slider_change)   
         self.H_Slider_min.valueChanged.connect(self.Slider_change)   
         self.S_Slider_max.valueChanged.connect(self.Slider_change)   
@@ -60,21 +70,14 @@ class Find_color_block(QtWidgets.QWidget, Ui_find_color_block):
         self.checkBox_Yellow.clicked.connect(self.Checkboxclick) 
         self.checkBox_Blue.clicked.connect(self.Checkboxclick) 
 
-        self.checkBox_Red.setCheckState(Qt.Checked)
-        # 设置默认为红色
-        self.hsv=red_hsv
-        self.H_Slider_max.setValue(self.hsv[0])
-        self.H_Slider_min.setValue(self.hsv[1])
-        self.S_Slider_max.setValue(self.hsv[2])
-        self.S_Slider_min.setValue(self.hsv[3])
-        self.V_Slider_max.setValue(self.hsv[4])
-        self.V_Slider_min.setValue(self.hsv[5])
+        self.print_hsv_data(self.hsv,self.red_hsv)
         
         print("初始化 ok")
         pass
 
-    def read_Slider(self):
-        
+    def print_hsv_data(self,temp,quanju):
+        print(temp)
+        print(quanju)        
         pass
     
     '''更新数据到控件'''
@@ -100,17 +103,27 @@ class Find_color_block(QtWidgets.QWidget, Ui_find_color_block):
         temp_hsv.append(self.V_Slider_max.value())
         temp_hsv.append(self.V_Slider_min.value())
 
-        if self.checkBox_Red.isChecked():
-            print("红色"+str(temp_hsv))
-        elif self.checkBox_Blue.isChecked():
-            print("蓝色"+str(temp_hsv))
-        elif self.checkBox_Yellow.isChecked():                
-            print("黄色"+str(temp_hsv))
-            pass
+        # if self.checkBox_Red.isChecked():
+        #     print("red_hsv"+str(self.red_hsv))
+        #     print("temp_hasv"+str(temp_hsv))
+        #     self.red_hsv=temp_hsv
+        #     print("red_hsv"+str(self.red_hsv))
+        #     print("temp_hasv"+str(temp_hsv))
 
-        # print(str(self.checkBox_Red.isChecked()))
-        # print(str(self.checkBox_Blue.isChecked()))
-        # print(str(self.checkBox_Yellow.isChecked()))
+        #     print("打印结束")
+
+        #     # print("红色"+str(temp_hsv))
+        # elif self.checkBox_Blue.isChecked():
+        #     print("蓝色"+str(temp_hsv))
+        # elif self.checkBox_Yellow.isChecked():                
+        #     print("黄色"+str(temp_hsv))
+        #     pass
+
+        print("红色"+str(self.checkBox_Red.isChecked()))
+        print("蓝色"+str(self.checkBox_Blue.isChecked()))
+        print("黄色"+str(self.checkBox_Yellow.isChecked()))
+        print(self.num)
+        self.num=self.num+1
 
         # print(hsv)
         self.TextEdit_hsv.clear()
@@ -129,7 +142,7 @@ class Find_color_block(QtWidgets.QWidget, Ui_find_color_block):
             self.TextEdit_hsv.clear()
             # 修改滑块位置
             # self.fill_data_to_Slider(color_dict["blue"])
-            self.fill_data_to_Slider(blue_hsv)
+            self.fill_data_to_Slider(self.blue_hsv)
             # 清除选中
             self.checkBox_Yellow.setCheckState(Qt.Unchecked)
             self.checkBox_Red.setCheckState(Qt.Unchecked)
@@ -137,7 +150,7 @@ class Find_color_block(QtWidgets.QWidget, Ui_find_color_block):
         elif name == self.checkBox_Red.objectName():
             self.TextEdit_hsv.clear()
             # self.fill_data_to_Slider(color_dict["red"])
-            self.fill_data_to_Slider(red_hsv)
+            self.fill_data_to_Slider(self.red_hsv)
 
             self.checkBox_Yellow.setCheckState(Qt.Unchecked)
             self.checkBox_Blue.setCheckState(Qt.Unchecked)
@@ -147,7 +160,7 @@ class Find_color_block(QtWidgets.QWidget, Ui_find_color_block):
             
             self.TextEdit_hsv.clear()
             # self.fill_data_to_Slider(color_dict["yellow"])
-            self.fill_data_to_Slider(yellow_hsv)
+            self.fill_data_to_Slider(self.yellow_hsv)
 
             self.checkBox_Blue.setCheckState(Qt.Unchecked)
             self.checkBox_Red.setCheckState(Qt.Unchecked)
